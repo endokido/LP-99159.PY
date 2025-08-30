8e24869b626f3bc6eb92edb4abb8592fcad137e4
endokido <130393612+endokido@users.noreply.github.com>
2025-08-30 02:27:02 -0300

    Delete exercicios directory

--- exercicios/voto.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system('cls')
-
-idade = int(input('Digite a sua idade: '))
-
-if idade < 18 or idade > 65:
-    print('Não é obrigado a votar.')
-else:
-    print('Obrigado a votar.')
